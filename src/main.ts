21fb9ffb081ccbaad1d15885eef945e36b5a0aba
ozoromo <lorenzo.llanes.romero@gmail.com>
2022-03-26 22:54:17 +0100

    Added event structure

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,28 @@ import atmosphereVertexShader from './shaders/atmosphere_vertex.glsl';
 // @ts-ignore
 import atmosphereFragmentShader from './shaders/atmosphere_fragment.glsl';
 
+//TODO add events with loop https://stackoverflow.com/questions/41338474/three-js-create-multiple-objects
+//TODO Fetch events from ACLED using api or data export
+// @ts-ignore
+class globalEvent {
+    public event: any;
+    public subEvent : any;
+    public actor : any;
+    public date : any;
+    public lat : any;
+    public long : any;
+    constructor(event, subEvent, actor, date, lat, long) {
+        this.event = event;
+        this.subEvent = subEvent;
+        this.actor = actor;
+        this.date = date;
+        this.lat = lat;
+        this.long = long;
+    }
+}
+
+
+
 //scene setup
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -21,9 +43,6 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
-
-var speed = 0.1 ;
-var delta = 0;
 
 //Stolen fom stack overflow
 // @ts-ignore
@@ -39,6 +58,7 @@ function placeObjectOnPlanet(object, lat, lon, radius) {
 }
 
 //Marker setup
+// @ts-ignore
 const marker = new THREE.Mesh(
     new THREE.SphereGeometry(0.07, 50, 50),
     new THREE.MeshBasicMaterial( {color: 0xea4f20} )
@@ -76,8 +96,8 @@ scene.add(planet);
 scene.add(atmosphere);
 
 
-
-
+var speed = 0.1 ;
+var delta = 0;
 
 function animate() {
     requestAnimationFrame( animate );
